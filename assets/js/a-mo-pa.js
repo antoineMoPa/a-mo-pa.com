370919e6d81a2d2135e77e78d4651aa4814aee0e
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2014-08-10 17:50:59 -0400

    Triangles fully integrated

--- assets/js/a-mo-pa.js
+++ assets/js/a-mo-pa.js
@@ -1,7 +1,6 @@
 $.ready(function(){
     happySquares(".a-mo-pa-canvas");
     happySquares2(".a-mo-pa-canvas-2");
-    happySquares3(".a-mo-pa-canvas-3");
     frequencies();
     spirals();
 });
@@ -39,12 +38,20 @@ function happySquares(element){
         draw({size:30},context,width,height,mouseX,mouseY,clicking);
     }  
     
-    setInterval(function(){
-        context.fillStyle = 
+    var interval;
+    
+    function start(){
+        interval = setInterval(function(){
+            context.fillStyle = 
             "rgba(100,100,100,"+(clicking?"0.1":"0.06")+")";
-        context.fillRect(0,0,width,height);
-    },100);
-
+            context.fillRect(0,0,width,height);
+        },100);
+    }
+    
+    canvas.onmouseenter = start;
+    canvas.onmouseleave = function(){
+        clearInterval(interval);
+    }
     
     function draw(settings,context,width,height,mouseX,mouseY,clicking){
         var size = parseInt(Math.random()*2)+settings.size;
@@ -85,7 +92,7 @@ function happySquares(element){
                 }
                 else{
                     context.fillStyle = 
-                        "rgba("+r+","+g+","+b+",0.1)";
+                        "rgba("+r+","+g+","+b+",0.3)";
                 }
                 context.fillRect(i,j,size,size);
             }
@@ -194,97 +201,6 @@ function happySquares2(element){
     }
 }
 
-function happySquares3(element){
-    var canvasSelector = element;
-    var canvas = $(canvasSelector).elements[0];
-    var context = canvas.getContext("2d");
-    
-    var width;
-    var height;
-
-    function adaptSize(){
-        width = $("body").width();
-        height = window.innerHeight
-        
-        canvas.width = width;
-        canvas.height = height;
-    }
-    
-    adaptSize();
-    $(window).on("resize",adaptSize);
-    
-    var mouseX = 0;
-    var mouseY = 0;
-    var clicking = false;
-
-    canvas.onmousemove = function(e){
-        e.preventDefault();
-        mouseX = e.clientX
-        mouseY = e.clientY + window.scrollY - canvas.offsetTop;
-        clicking = (e.buttons == 1);
-        draw({size:10},context,width,height,mouseX,mouseY,clicking);
-        draw({size:30},context,width,height,mouseX,mouseY,clicking);
-        draw({size:50},context,width,height,mouseX,mouseY,clicking);
-    }  
-    
-    function draw(settings,context,width,height,mouseX,mouseY,clicking){
-        var size = settings.size;
-        var currentMax = 0;
-        var i = 0;
-        var j = 0;
-        var step = 0;
-        var dist;
-        var limit;
-        var sizeMultiplier;
-        
-        while(currentMax < width*2 || currentMax < height*2){
-            var paint,r,g,b;
-            
-            sizeMultiplier = 2;
-            limit = 
-                size*sizeMultiplier*Math.random();
-            
-            dist = distance(i,j,mouseX,mouseY);
-            
-            if(dist < limit){
-                if(step % 2 == 0){
-                    paint = parseInt(currentMax/width*255*Math.random());
-                    r = (200-parseInt(paint/5));
-                    g = paint;
-                    b = parseInt(100*Math.random());
-                }
-                else{
-                    paint = parseInt(currentMax/width*255);
-                    r = parseInt(i/width*255);
-                    g = parseInt(i/width*255*Math.random());
-                    b = 40+parseInt(Math.random())*155-parseInt(i/width*40);
-                }
-                if(clicking){
-                    context.fillStyle = 
-                        "rgba("+r+","+g+","+b+",0.4)";
-                }
-                else{
-                    context.fillStyle = 
-                        "rgba("+r+","+g+","+b+",0.1)";
-                }
-                context.fillRect(i,j,size,size);
-            }
-            
-            j += size;
-            i -= size;
-            
-            // line change
-            if(j > currentMax){
-                j = 0;
-                i = currentMax += size;
-                step++;
-            }
-        }
-    }
-}
-
-
-
 function distance(x1,y1,x2,y2){
     return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 }
@@ -302,14 +218,8 @@ function frequencies(){
     var ctx = c.getContext("2d");
     var interval;
     
-    c.width = document.body.clientWidth;
-    c.height = 400;
-    
-    adaptSize(c);
-    $(window).on("resize",function(){
-        adaptSize(c);
-    });
-    
+    c.width = 1280;
+    c.height = 500;
     
     var j = 0;
     var demiheight = c.height/2;
@@ -377,14 +287,8 @@ function spirals(c,ctx){
     var ctx = c.getContext("2d");
     var interval;
     
-    c.width = document.body.clientWidth;
+    c.width = 400;
     c.height = 400;
-    
-    adaptSize(c);
-    $(window).on("resize",function(){
-        adaptSize(c);
-    });
-
     
     map = new Map(c,ctx);
         
--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -21,35 +21,68 @@ $.ready(function(){
     
     //x,y,z,materialid
     renderer.objects = {};
-
-    renderer.addQuad(
-        [0,0,5],
-        [0,1,5],
-        [1,1,5],
-        [1,0,6]
-    );
-    renderer.addQuad(
-        [0,0,5],
-        [0,0,2],
-        [1,0,2],
-        [1,0,6]
-    );
-    renderer.addQuad(
-        [0,0,5],
-        [0,0,2],
-        [1,0,2],
-        [1,0,6]
-    );
     
     renderer.materials = [
         "rgba(55,10,50,0.9)",
         "rgba(0,55,50,0.9)"
     ];
+   
+    var lastRender = new Date().getTime();
+    var x = c.width / 2;
+    var y = c.height;
     
-    renderer.renderFrame();
+    
+    var container = document.querySelectorAll(".title-canvas-container")[0];
+    container.onmousemove = c.onmousemove = function(e){
+        x = 0.7 * e.clientX;
+        y = e.clientY;
+    }
+    
+    var interval;
+    
+    function start(){
+        interval = setInterval(replaceTris,80);
+    }
+    
+    container.onclick = function(){
+        container.querySelectorAll("h1")[0].style.opacity = 0;
+    }
+    container.onmouseenter = start;
+    container.onmouseleave = function(){
+        clearInterval(interval);
+    }
+    
+    var sizeX = 30;
+    var sizeZ = 30;
+    
+    function replaceTris(){
+        var date = new Date().getTime();
+        renderer.tris = [];
+        for(var i = 0; i < sizeX; i++){
+            for(var j = 0; j < sizeZ; j++){
+                var yOffset = 
+                    1 *
+                    Math.sin( 
+                        x / c.width * sizeX + 3 * i  * 180
+                    )
+                    + (6 - 10 * y / c.height);
+                var zYOffset = Math.cos( 3 * j + date % 20000 / 500);
+                
+                renderer.tris.push(
+                        -sizeX/3 + i + j / 3, -0.4 + yOffset + zYOffset, 4 + j * 1.2,
+                        -sizeX/3 + i + j / 3, 0 + yOffset, 5 + zYOffset + j * 1.3,
+                        -sizeX/3 + i + j / 3, -1 + yOffset + zYOffset, 4 + j * 1.3
+                );
+            }   
+        }
+        renderer.renderFrame();
+    }
+    for(var i = 0; i < 10; i++){
+        replaceTris();
+    }
 });
 
-function Object3D = function(vertexes,materials){
+function Object3D(vertexes,materials){
     
 }
 
@@ -76,7 +109,7 @@ Renderer.prototype.pointToCamera = function(points){
     // To do: ensure ints stay int
     // Invert Y axis, scale X and Y axis
     
-    var scale = 1 / (0.6 * points[2]);
+    var scale = 1 / (0.7 * points[2]);
     
     var offsetX = (this.canvas.height - scale * this.canvas.height) / 2;
     var offsetY = (this.canvas.height - scale * this.canvas.height) / 2;
@@ -88,14 +121,14 @@ Renderer.prototype.pointToCamera = function(points){
 };
 
 Renderer.prototype.renderFrame = function(){
-    this.ctx.fillStyle = "#fff";
+    this.ctx.fillStyle = "rgba(255,255,255,0.3)";
     this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
 
     for(var i = 0; i < this.tris.length; i+=9){
         this.ctx.beginPath();
         
         //this.ctx.fillStyle = this.materials[this.tris[i+9]];
-        this.ctx.fillStyle = this.materials[0];
+        this.ctx.fillStyle = "rgba(150,0,0,0.3)";
         this.ctx.moveTo.apply(this.ctx,this.pointToCamera(this.tris.slice(i,i+3)));
         this.ctx.lineTo.apply(this.ctx,this.pointToCamera(this.tris.slice(i+3,i+6)));
         this.ctx.lineTo.apply(this.ctx,this.pointToCamera(this.tris.slice(i+6,i+9)));
