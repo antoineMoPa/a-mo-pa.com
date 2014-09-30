9cae0e70e400e835c7548145209947d681e8a830
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2014-09-29 21:34:33 -0400

    Add wave drawing tool

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -17,6 +17,7 @@ $.ready(function(){
         );
     })
     
+    initArrayDrawer();
 });
 
 function Sound(){
@@ -61,4 +62,114 @@ Sound.prototype.print = function(message){
     );
     this.log.elements[0].scrollTop = 
         this.log.elements[0].scrollHeight;
+}
+
+function initArrayDrawer(){
+    var canvas = document
+        .querySelectorAll("canvas.array-drawer")[0];
+    
+    var ctx = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+    
+    var array = [];
+    
+    resetSin();
+    
+    redraw();
+    
+    canvas.onmousemove = eventHandler;
+    canvas.onmousedown = eventHandler;
+    
+    var timeout = null;
+    var minTimeBetweenFrames = 33;
+
+    function eventHandler(e){
+        var x = (e.pageX - canvas.offsetLeft) / canvas.width;
+        var y = 1 - 2 * 
+            (e.pageY - canvas.offsetTop) / 
+            canvas.height;
+        if(e.buttons == 1){
+            var index = parseInt(x*array.length);
+            
+            var smoothness = 30;
+
+            var i = index - smoothness;
+            var diff;
+            
+            
+            
+            for(; i <= index + smoothness; i++){
+                if(i < 0 || i > array.length){
+                    continue;
+                }
+                diff = Math.abs(index - i);
+                array[i] = ((smoothness-diff) * y +
+                            (smoothness+diff) * array[i]) 
+                    / (2 * smoothness);
+            }
+            
+        }
+        var time = new Date().getTime();
+        
+        if(timeout != null && time < minTimeBetweenFrames){
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(function(){
+            redraw();
+            timeout = null;
+            lastDraw = new Date().getTime();;
+        },minTimeBetweenFrames);
+        
+    }
+    
+    
+    function redraw(){
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0,0,width,height);
+        for(var i = 0; i < array.length; i++){
+            ctx.fillStyle = "#000";
+            var yPos = height/2 - (height/2) * array[i];
+            ctx.fillRect(i,yPos,1,height - yPos);
+        }
+        document
+            .querySelectorAll(".array-drawer-content")[0]
+            .innerHTML = array;
+    }
+    
+    document.querySelectorAll(".reset-to-sin")[0]
+        .onclick = function(){
+            resetSin();
+        }
+    
+    document.querySelectorAll(".reset-to-cos")[0]
+        .onclick = function(){
+            resetCos();
+        }
+
+    document.querySelectorAll(".reset-to-flat")[0]
+        .onclick = function(){
+            resetFlat();
+        }
+    
+    function resetCos(){
+        for(var i = 0; i < width; i++){
+            array[i] = Math.cos(2*Math.PI*i/width);
+        }
+        redraw();
+    }
+
+    function resetSin(){
+        for(var i = 0; i < width; i++){
+            array[i] = Math.sin(2*Math.PI*i/width);
+        }
+        redraw();
+    }
+    
+    function resetFlat(){
+        for(var i = 0; i < width; i++){
+            array[i] = 0;
+        }
+        redraw();
+    }
 }
