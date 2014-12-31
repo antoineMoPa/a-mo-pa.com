3b7cc865875b65811fd6bbf8036f46d474c2fd41
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2014-12-31 01:02:28 -0500

    attempt to make waves more physically meaningful

--- experiments/js/waves.js
+++ experiments/js/waves.js
@@ -9,19 +9,6 @@ function initWaves(){
     var w = canvas.width;
     var h = canvas.height;
     
-    var potentials = bidimentionnalArray(w, h, 0);
-    var momentums = bidimentionnalArray(w, h, 0);
-    
-    var animationImages = [];
-    
-    initValue(150,100,40);
-    initValue(250,100,40);
-    draw();
-    
-    function initValue(i,j,value){
-        potentials[i][j] = value;
-    }
-        
     playBtn.onclick = play;
     
     var animationInterval = null;
@@ -44,50 +31,61 @@ function initWaves(){
         }
     }
     
-    /*
-      
-      2 things are happening here:
-      
-      1- potential is being shared with neighboors
-      2- each particle has its momentum
-      
-     */
+    var heights = bidimentionnalArray(w, h, 0);
+    var speeds = bidimentionnalArray(w, h, 0);
+    var springConstant = 10000;
+    var mass = 1;
+    var interval = 0.2;
     
-    function iterate(){
+    initValue(150,100,100,1);
+    
+    draw();
+    
+    function initValue(i,j,radius,value){
+        for(var k = i - radius; k < i + radius; k++){
+            for(var l = j - radius; l < j + radius; l++){
+                var dist = d(i,j,k,l);
+                if(dist < radius){
+                    heights[k][l] = value * (1 - dist / radius);
+                }                
+            }
+        }
+        function d(x1,y1,x2,y2){
+            return Math.sqrt(Math.pow(y2-y1,2)+Math.pow(x2-x1,2));
+        }
+    }
+    
+    function iterate(){        
         var factor = 1/4;
-                
-        // equilibrate potentials
+        
+        // equilibrate heights
         for(var i = 1; i < w - 1; i++){
-            for(var j = 1; j < h - 1; j++){                
-                // take potentials into account
-                //equilibrate(i,j, i-1 , j-1, factor);
+            for(var j = 1; j < h - 1; j++){            
                 equilibrate(i,j, i   , j-1, factor);
-                //equilibrate(i,j, i+1 , j-1, factor);
                 equilibrate(i,j, i+1 , j  , factor);
-                                            
-                //equilibrate(i,j, i+1 , j+1, factor);
                 equilibrate(i,j, i   , j+1, factor);
-                //equilibrate(i,j, i-1 , j+1, factor);
-                equilibrate(i,j, i-1 , j  , factor);                
-            }            
+                equilibrate(i,j, i-1 , j  , factor);
+                // Corners
+                /*equilibrate(i,j, i-1 , j-1, 0.01 * factor);
+                equilibrate(i,j, i+1 , j-1, 0.01 * factor);
+                equilibrate(i,j, i+1 , j+1, 0.01 * factor);                
+                equilibrate(i,j, i-1 , j+1, 0.01 * factor);                
+                */
+            }     
         }
         
         for(var i = 1; i < w - 1; i++){
-            for(var j = 1; j < h - 1; j++){                
-                potentials[i][j] += momentums[i][j];
+            for(var j = 1; j < h - 1; j++){
+                // f = ma
+                heights[i][j] += speeds[i][j];
             }
         }
         
         function equilibrate(i, j, k, l, factor){
-            var new1 = 
-                (1 - factor) * potentials[i][j] + 
-                factor * potentials[k][l];
-            var new2 = 
-                (1 - factor) * potentials[k][l] + 
-                factor * potentials[i][j];
+            var deltaH = heights[i][j] - heights[k][l];
             
-            momentums[i][j] += new1 - potentials[i][j];
-            momentums[k][l] += new2 - potentials[k][l];
+            speeds[i][j] -= factor * deltaH;
+            speeds[k][l] += factor * deltaH;
         }        
     }
     
@@ -95,18 +93,20 @@ function initWaves(){
         ctx.clearRect(0,0,w,h);
         var data = ctx.createImageData(w,h);
         for(var i = 0; i < w; i++){
-            for(var j = 0; j < h; j++){                
+            for(var j = 0; j < h; j++){
                 var index = 4 * (j * w + i);
                 // Set color
-                var red = green = blue = Math.abs(
-                    parseInt(
-                        1-(Math.pow(1-(potentials[i][j] / 10),40))
-                            * 255)
-                );                
-                
-                data.data[index] = red;                
-                data.data[index + 1] = red;
-                data.data[index + 2] = red;
+                var red =
+                    Math.abs(
+                        parseInt(heights[i][j] * 255)
+                    );
+                var green = 
+                    Math.abs(
+                        parseInt(30*speeds[i][j] * 255)
+                    );
+                data.data[index + 0] = red;
+                data.data[index + 1] = green;
+                data.data[index + 2] = 0;
                 data.data[index + 3] = 255;
             }
         }
