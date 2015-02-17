49370f84541ea6eda77e416f98213da18bd69465
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-02-17 13:21:41 -0500

    concave lines

--- stuff/lines.js
+++ stuff/lines.js
@@ -14,7 +14,7 @@ var lastX = 0;
 var lastY = 0;
 
 var points = [];
-
+draw();
 can.onclick = function(e){
     x = e.clientX - can.offsetLeft;
     y = e.clientY - can.offsetTop;
@@ -26,20 +26,36 @@ function draw(){
     ctx.fillStyle = "#fff";
     ctx.fillRect(0,0,w,h);
     ctx.fillStyle = "rgba(0,0,0,0.9)";
-    for(var i = 1; i < points.length; i++){   
+    for(var i = 0; i < points.length; i++){
+        ctx.fillRect(points[i][0]-2, points[i][1]-2, 4, 4);   
+    }
+    for(var i = 1; i < points.length - 1; i+=2){   
         var res = 20;
         // point
         var p = points[i];
         // lastpoint
         var lp = points[i-1];
-        var dx = p[0] - lp[0];
-        var dy = p[1] - lp[1];
-        
-        ctx.fillRect(p[0]-2, p[1]-2, 4, 4);        
+        var np = points[i+1];             
         
         for(var j = 0; j < res ; j++){             
-            var m = lp[0] + j / res * dx;
-            var n = lp[1] + j / res * dy;            
+            var k = j/res;
+            var m = k * np[0] + (1-k) * lp[0];
+            var n = k * np[1] + (1-k) * lp[1];
+            if(k < 0.5){
+                var u = 2*k
+                var q = (1-u) * lp[0] + (u) * p[0];
+                var r = (1-u) * lp[1] + (u) * p[1];
+                m = u * q + (1-u) * m;
+                n = u * r + (1-u) * n;
+            }
+            if(k >= 0.5){
+                var u = 1-2*(k-0.5);
+                var q = (1-u) * np[0] + (u) * p[0];
+                var r = (1-u) * np[1] + (u) * p[1];
+                m = u * q + (1-u) * m;
+                n = u * r + (1-u) * n;
+            }
+            
             ctx.fillRect(m-1, n-1, 2, 2);
         }
     }
