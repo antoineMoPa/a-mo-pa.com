65ac1fd3a53d6889d690d0cb623bdc1fb105e447
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-02-16 23:03:47 -0500

    separate draw function

--- stuff/lines.js
+++ stuff/lines.js
@@ -16,28 +16,33 @@ var lastY = 0;
 var points = [];
 
 can.onclick = function(e){
-    ctx.fillStyle = "rgba(0,0,0,0.9)"
     x = e.clientX - can.offsetLeft;
     y = e.clientY - can.offsetTop;
-    
-    var res = 20;
-    
-    var dx = x - lastX;
-    var dy = y - lastY;
-    
-    ctx.fillRect(x-2, y-2, 4, 4);
-    
-    for(var i = 0; i < res ; i++){        
-        
-        var m = lastX + i / res * dx;
-        var n = lastY + i / res * dy;
-        
-        ctx.fillRect(m-1, n-1, 2, 2);
-    }
-   
-
-    lastX = x;
-    lastY = y;
+    points.push([x,y]);
+    draw();
 };
+
+function draw(){
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0,0,w,h);
+    ctx.fillStyle = "rgba(0,0,0,0.9)";
+    for(var i = 1; i < points.length; i++){   
+        var res = 20;
+        // point
+        var p = points[i];
+        // lastpoint
+        var lp = points[i-1];
+        var dx = p[0] - lp[0];
+        var dy = p[1] - lp[1];
+        
+        ctx.fillRect(p[0]-2, p[1]-2, 4, 4);        
+        
+        for(var j = 0; j < res ; j++){             
+            var m = lp[0] + j / res * dx;
+            var n = lp[1] + j / res * dy;            
+            ctx.fillRect(m-1, n-1, 2, 2);
+        }
+    }
+}
 
 var points = [];
