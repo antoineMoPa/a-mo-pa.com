4e26e602271939ea5a6a79bedb3cab7cc452d535
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-03-05 21:19:32 -0500

    nice stuff

--- stuff/js/message.js
+++ stuff/js/message.js
@@ -5,6 +5,9 @@ ctx = can.getContext("2d");
 w = can.width = 1000;
 h = can.height = 800;
 
+var message = window.location.hash.slice(1);
+
+can.style.height = "100%";
 can.style.width = "100%";
 
 ctx.fillStyle = "rgba(100,100,100,1)";
@@ -13,6 +16,14 @@ ctx.fillRect(0,0,w,h);
 var int = setInterval(update,33);
 
 var angle = 0;
+
+var normal_size = 200 - 20 * message.length;
+
+if(normal_size < 20){
+    normal_size = 20;
+}
+
+var size = 500;
 
 var r = 0.1;
 var g = 0.3;
@@ -25,21 +36,30 @@ function update(){
     ctx.save();
     ctx.translate(w/2,h/2)
     ctx.rotate(angle);
-    ctx.fillStyle = "#000";
-    ctx.font = '200px Sans-serif'
-    ctx.fillText("JTM",-98,48);
-    ctx.font = '200px Sans-serif'
+    ctx.font = parseInt(size)+'px Sans-serif'
     ctx.fillStyle = "rgb("+col(r)+","+col(g)+","+col(b)+")";
-    ctx.fillText("JTM♥",-100,50);
+    ctx.fillText(message,-100,50);
     ctx.restore();
 
     function col(color){
-        return parseInt(255 * Math.sin(color*30));
+        return parseInt(255 * Math.sin(color*20));
     }
-    
-    angle += 0.06;
 
+    if(size > normal_size){
+        angle += 0.2;
+    } else {
+        angle += 0.02;
+    }
     r += 0.003;
     g += 0.005;
     b += 0.0045;
+    if(size > normal_size){
+        if(size > 200){
+            size-=3;
+        } else {
+            size-=1;
+        }
+    } else {
+        size = normal_size;
+    }
 }
