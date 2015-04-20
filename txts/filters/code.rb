4df856a7adabbe8bad1e91faa65071bca3ea5a5d
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-04-20 00:23:09 -0400

    remove <br> at begining of snippets

--- txts/filters/code.rb
+++ txts/filters/code.rb
@@ -3,9 +3,11 @@ class Code
     codetypes = ["terminal","code","pre"]
     codetypes.each do |type|
       string = string
-        .gsub(
-              /\[#{type}\]\n?(.+?)\[\/#{type}\]/m) {
-        content = $1.gsub("\n","[real-line-break]")
+               .gsub(
+                 /\[#{type}\]\n?(.+?)\[\/#{type}\]/m) {
+        content = $1
+        content = content.gsub("\n","[real-line-break]")
+        content = content.gsub(/^<br>/,"")
         "</p><pre class='#{type}'>#{content}</pre><p>"
       }
     end
