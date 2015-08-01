491d0029ff9c76a4e50d78c1cf615f19cfd62325
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-08-01 19:32:58 -0400

    space objects

--- stuff/fluid/fluid.js
+++ stuff/fluid/fluid.js
@@ -4,35 +4,33 @@ var ctx = can.getContext('2d');
 var w = can.width = 500;
 var h = can.height = 500;
 var lastmousemove = {};
-
-/**
-   PARTICLES SPECIFICATION
-   
-   0: x
-   1: y
-   2: vx
-   3: vy
-   4: (auto) absolute speed
-   5: (auto) absolute angle
-   6: density (future)
-   7: temperature (future)
- 
-*/
-
+/* see particle_spec.txt */
 var particles = [];
 
 can.onmousemove = function(e){
     lastmousemove = e;
 }
 
-function new_particle(x,y){
-    particles.push([x,y,0,0]);
-}
-
 for(var i = 0; i < 10; i++){
     for(var j = 0; j < 10; j++){
 	new_particle(i*30+30,j*30+30);
     }
+}
+
+function new_particle(x,y){
+    particles.push([
+	x,
+	y,
+	0,   // vx
+	0,   // vy
+	0,   // speed
+	0,   // angle
+	0.1, // fx
+	0.1, // fy
+	0,   // ax
+	0,   // ay
+	0.2  // mass
+    ]);
 }
 
 setInterval(anim,100)
@@ -43,13 +41,58 @@ function anim(){
 }
 
 function calc(){
-    var kcs = kcursorspeed = 0.1;
+    var kcs = kcursorspeed = 1;
     var pointerX = lastmousemove.pageX || 0;
     var pointerY = lastmousemove.pageY || 0;
     for(var i = 0; i < particles.length;i++){
-	// set speed according to pointer
-	particles[i][2] = kcs * (pointerX - particles[i][0]);
-	particles[i][3] = kcs * (pointerY - particles[i][1]);
+
+	// reset forces
+	particles[i][6] = 0;
+	particles[i][7] = 0;
+
+	var deltaX = pointerX - particles[i][0];
+	var deltaY = pointerY - particles[i][1];
+	var d = Math.sqrt(
+	    Math.pow(deltaX,2) +
+		Math.pow(deltaY,2)
+	);
+	// towards pointer force
+	particles[i][6] += kcs * deltaX / Math.pow(
+	    d,2
+	);
+	particles[i][7] += kcs * deltaY / Math.pow(
+	    d,2
+	);
+    }
+    
+    // calculate acceleration, speed, update position
+    for(var i = 0; i < particles.length;i++){
+	//
+	// F = ma => a = f/m
+	// (yeah)
+
+	// x
+	particles[i][8] = particles[i][6] / particles[i][10];
+	// y
+	particles[i][9] = particles[i][7] / particles[i][10];
+	
+	//
+	// v = v + a
+	//
+
+	// x
+	particles[i][2] = particles[i][2] + particles[i][8];
+	// y
+	particles[i][3] = particles[i][3] + particles[i][9];
+
+	//
+	// p = p + v
+	//
+	
+	// x
+	particles[i][0] = particles[i][0] + particles[i][2];
+	// y
+	particles[i][1] = particles[i][1] + particles[i][3];
 	
 	// find final speed
 	// v = sqrt(vx ^ 2 + vy ^ 2) [pythagorus]
@@ -75,7 +118,6 @@ function calc(){
 			(particles[i][2])
 		)
 	    ;
-
 	}
     }
 }
