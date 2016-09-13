0e0d95d8a8c6840d0d2ce087f0fe6cdf98a88fdd
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-09-13 15:01:29 -0400

    update text

--- main.js
+++ main.js
@@ -3,22 +3,3 @@
 // GNU GPL v3 or later
 // http://www.gnu.org/copyleft/gpl.html
 
-var titles = document.querySelectorAll("h1,h2,h3");
-
-for(var i = 0; i < titles.length; i++){
-    init_title(titles[i]);
-}
-
-function init_title(title){
-    var vals = title.innerHTML.split("|");
-    var current = 0;
-    change_title();
-    
-    function change_title(){
-        title.innerHTML = vals[current % vals.length];
-        current++;
-    }
-    
-    setInterval(change_title,Math.random()*1000+1000);
-}
-
